650ebe39c590d4e65502a60e990da47e83c55661
ParanoicMadara <qqqaaawwwssseeedddrrr@mail.ru>
2024-08-01 00:49:19 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 'Madara', 'Itachi', 'Sasuke', 'Shin'
+print (immutable_var)
+#immutable_var [0] = 'Naruto' неизменяемый список, замена элементов невозможна
+mutable_list = (['Sakura', 'Ino'], ['Gats', 'Grifit'])
+mutable_list[0][1] = 'Budda'
+print(mutable_list)
